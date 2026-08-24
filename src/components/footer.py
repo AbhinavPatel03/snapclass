38ce4b634e6c1b0b0755f24d3d9e17e28ff087b4
Abhinav Patel <abhinavpatel9508@gmail.com>
2026-08-24 22:14:03 +0530

    homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def footer_home():
+
+
+    st.markdown(f"""
+        <div style="display: flex;justify-content: center; margin-top: 2rem; gap: 6px; items-align: center;">
+            <p style="font-weight: bold; color: white;"> Created with ❤️ by ABHINAV PATEL</p>
+           
+        </div>  
+    """, unsafe_allow_html=True)
