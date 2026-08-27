cfe6fd04cfdace1d71b7561d6e249e83d93bcf66
Abhinav Patel <abhinavpatel9508@gmail.com>
2026-08-27 19:55:14 +0530

    Finishing Touches

--- src/components/footer.py
+++ src/components/footer.py
@@ -9,3 +9,14 @@ def footer_home():
            
         </div>  
     """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+
+
+    st.markdown(f"""
+        <div style="display: flex;justify-content: center; margin-top: 2rem; gap: 6px; items-align: center;">
+            <p style="font-weight: bold; color: black;"> Created with ❤️ by ABHINAV PATEL</p>
+           
+        </div>  
+    """, unsafe_allow_html=True)
